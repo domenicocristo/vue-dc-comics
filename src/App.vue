801f53b9406f76137102827c3055a26bd465428d
MyUsername <my@email.com>
2021-11-24 15:46:51 +0100

    creo ciclo for header

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
 
 <script>
 import Header from './components/Header.vue'
-import Header from './components/Main.vue'
-import Header from './components/Footer.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,64 @@
 <template>
   <header>
-
+      <img src="../assets/img/dc-logo.png" alt="logo-header">
+      <nav>
+        <ul>
+            <li v-for="link, i in links" :key="i">
+                <a href="#">{{link.text}}</a>
+            </li>
+        </ul>
+      </nav>
   </header>
 </template>
 
 <script>
 export default {
   name: 'Header',
+  date() {
+      return {
+          links: [
+              {
+                  text: "CHARACTERS"
+              },
+
+              {
+                  text: "COMICS"
+              },
+
+              {
+                  text: "MOVIES"
+              },
+
+              {
+                  text: "TV"
+              },
+
+              {
+                  text: "GAMES"
+              },
+
+              {
+                  text: "COLLECTIBLES"
+              },
+
+              {
+                  text: "VIDEOS"
+              },
+
+              {
+                  text: "FANS"
+              },
+
+              {
+                  text: "NEWS"
+              },
+
+              {
+                  text: "SHOP"
+              }
+          ]
+      }
+  }
 }
 </script>
 
